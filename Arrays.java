d7f211552d9f584a0ed6883225dccd23778ceadd
Suri Hui <suri.hui@mrjaffesclass.com>
2023-09-15 11:24:07 -0700

    I did Arrays right

--- Arrays.java
+++ Arrays.java
@@ -2,9 +2,19 @@ public class Arrays
 {
     public static void main(String[] args)
     {
+        System.out.println(" ") ;
+        int iterate;
         String[] strArray = {"Alpha","Bravo","Charlie","Delta","Echo"};
-        for (int i = 1; i < strArray.length; i++) {
+        for (iterate = 0; iterate < strArray.length; iterate++) {
             System.out.println(strArray[iterate]);
+        }
+        for (iterate = 0; iterate < strArray.length; iterate++) {
+            System.out.print(strArray[iterate]);
+        }
+        System.out.println(" ");
+        for (iterate = 0; iterate < strArray.length; iterate++) {
+            System.out.print(strArray[iterate]);
+            System.out.print("-");
         }
     }
 }
